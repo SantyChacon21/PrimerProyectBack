521da5a41ff444c76d9dba90bbd7937187585ca1
SantyChacon21 <santychacon21@gmail.com>
2023-01-31 19:57:07 -0300

    agregado filter fs y cls codigo

--- app.js
+++ app.js
@@ -8,42 +8,30 @@ const PORT = 8080
 
 app.use(express.urlencoded({extended:true}))
 
-const products =[
-    {nombre: 'producto1', id:'1', description: 'este es un producto 1'},
-    {nombre: 'producto2', id:'2', description: 'este es un producto 2'},
-    {nombre: 'producto3', id:'3', description: 'este es un producto 3'},
-    {nombre: 'producto4', id:'4', description: 'este es un producto 4'},
-    {nombre: 'producto5', id:'5', description: 'este es un producto 5'},
-    {nombre: 'producto6', id:'6', description: 'este es un producto 6'},
-    {nombre: 'producto7', id:'7', description: 'este es un producto 7'},
-    {nombre: 'producto8', id:'8', description: 'este es un producto 8'},
-    {nombre: 'producto9', id:'9', description: 'este es un producto 9'}
-]
-/* Traemos todos los productos */
-/* app.get('/products',(req, res)=>{
-    console.log(req.query)
-    const {limit} = req.query
-    const listado =  products.slice(0, limit)
-    res.send(listado)
-}) */
-
-/* Filtramos por id */
-app.get('/products/:id',(req, res)=>{
-    const {id} = req.params
-    const product = products.find(idProduct => idProduct.id === id)
-    if(!product){
-        return res.send(`No existe el producto con el ID ${id}`)
+const filterFile = async ()=>{
+    try {
+        const data = await fs.promises.readFile('./products.json','utf-8' )
+        const dataJs = JSON.parse(data)
+        app.get('/products/:id',(req, res)=>{
+            const {id} = req.params
+            const dataFilter = dataJs.find(idProduct => idProduct.id === id)
+            if(!dataFilter){
+                return res.send(`No existe el producto con el ID ${id}`)
+            }
+            res.send(dataFilter)
+        })
+    } catch (error) {
+        console.log(error)
     }
-    res.send(product)
-})
+}
+
 
 const leerArchivo = async ()=>{
     try {
-       let data = await fs.promises.readFile('./products.json','utf-8' )
-       let dataJs = JSON.parse(data)
+       const data = await fs.promises.readFile('./products.json','utf-8' )
+       const dataJs = JSON.parse(data)
        app.get('/products',(req, res)=>{
         const {limit} = req.query
-        console.log(req.query)
         const dataLimit = dataJs.slice(0, limit)
         res.send(dataLimit)
     })
@@ -53,24 +41,8 @@ const leerArchivo = async ()=>{
 }
 
 leerArchivo()
+filterFile()
 
-
-
-/* app.get('/products', (req, res)=>{
-    console.log(req.products)
-    const {limit} = req.products
-    res.send({
-        limit
-    })
-}) */
-
-
-/* clase 7 */
-
-/* app.get('/query', (req, res)=>{
-    console.log(req.query)
-    res.send(`query`)
-}) */
 
 app.listen(PORT, err=>{
     if (err) console.log(err)
