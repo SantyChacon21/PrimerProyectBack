0cefd0e3216bfaa1118e96dcf6be15fcc689aa92
SantyChacon21 <santychacon21@gmail.com>
2023-02-02 23:52:05 -0300

    correcciones

--- app.js
+++ app.js
@@ -8,40 +8,36 @@ const PORT = 8080
 
 app.use(express.urlencoded({extended:true}))
 
-const filterFile = async ()=>{
-    try {
+
         const data = await fs.promises.readFile('./products.json','utf-8' )
         const dataJs = JSON.parse(data)
-        app.get('/products/:id',(req, res)=>{
+        app.get('/products/:id',async(req, res)=>{
+            try{
             const {id} = req.params
             const dataFilter = dataJs.find(idProduct => idProduct.id === id)
             if(!dataFilter){
                 return res.send(`No existe el producto con el ID ${id}`)
             }
             res.send(dataFilter)
+        }catch(error){
+            console.log(error)
+        }
         })
-    } catch (error) {
-        console.log(error)
-    }
-}
 
 
-const leerArchivo = async ()=>{
-    try {
-       const data = await fs.promises.readFile('./products.json','utf-8' )
-       const dataJs = JSON.parse(data)
-       app.get('/products',(req, res)=>{
+       
+       app.get('/products', async(req, res)=>{
+        try{
+        const data = await fs.promises.readFile('./products.json','utf-8' )
+        const dataJs = JSON.parse(data)
         const {limit} = req.query
         const dataLimit = dataJs.slice(0, limit)
         res.send(dataLimit)
+        }catch(error){
+            console.log(error)
+        }
     })
-    } catch (error) {
-        console.log(error)
-    }
-}
 
-leerArchivo()
-filterFile()
 
 
 app.listen(PORT, err=>{
